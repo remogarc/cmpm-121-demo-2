464ceaf3b4e8d1d1873a557b4270e6de74930d0e
Reese Garcia <remogarc@ucsc.edu>
2023-10-17 15:37:41 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ header.innerHTML = gameName;
 app.append(header);
 
 const canvasSize = 256;
+const origin = 0;
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d")!;
@@ -21,32 +22,65 @@ clearButton.innerHTML = "clear";
 app.append(clearButton);
 
 ctx.fillStyle = "white";
-ctx.fillRect(0, 0, canvasSize, canvasSize);
+ctx.fillRect(origin, origin, canvasSize, canvasSize);
+
+const drawingChanged = new Event("drawing-changed");
+
+interface Point {
+  x: number;
+  y: number;
+}
+let points: Point[][] = [];
 
 const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("drawing-changed", () => {
+  updateCanvas();
+});
 
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+  points.push([]);
+  canvas.dispatchEvent(drawingChanged);
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    const offset = 1;
+    const currentPoint: Point = { x: cursor.x, y: cursor.y };
+    points[points.length - offset].push(currentPoint);
   }
+  canvas.dispatchEvent(drawingChanged);
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
 });
 
+function updateCanvas() {
+  ctx.clearRect(origin, origin, canvasSize, canvasSize);
+  ctx.fillRect(origin, origin, canvasSize, canvasSize);
+
+  if (points) {
+    points.forEach((item) => {
+      ctx.beginPath();
+      const [first, ...otherPoints]: Point[] = item;
+      if (first) {
+        ctx.moveTo(first.x, first.y);
+      }
+      for (const { x, y } of otherPoints) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    });
+  }
+}
+
 clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvasSize, canvasSize);
-  ctx.fillRect(0, 0, canvasSize, canvasSize);
+  points = [];
+  canvas.dispatchEvent(drawingChanged);
 });
