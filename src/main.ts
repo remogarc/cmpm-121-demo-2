bea078be436ba5b18659f807eca5449e3d551fe2
Reese Garcia <remogarc@ucsc.edu>
2023-10-16 19:57:33 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Reese's game";
+const gameName = "Creative Canvas";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d")!;
+app.append(canvas);
+ctx.canvas.width = 256;
+ctx.canvas.height = 256;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 256, 256);
