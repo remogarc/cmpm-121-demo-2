686d867aa1b5cea2973f9a0373d560768dc2ce73
Reese Garcia <remogarc@ucsc.edu>
2023-10-17 10:20:46 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,50 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Creative Canvas";
-
 document.title = gameName;
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+const canvasSize = 256;
+
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d")!;
 app.append(canvas);
-ctx.canvas.width = 256;
-ctx.canvas.height = 256;
+ctx.canvas.width = canvasSize;
+ctx.canvas.height = canvasSize;
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
 
 ctx.fillStyle = "white";
-ctx.fillRect(0, 0, 256, 256);
+ctx.fillRect(0, 0, canvasSize, canvasSize);
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvasSize, canvasSize);
+  ctx.fillRect(0, 0, canvasSize, canvasSize);
+});
